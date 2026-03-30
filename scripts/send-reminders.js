51fc91b74d2884828d2d14d4347448d1798c4dd0
rtabbara1 <rtabbara1@gmail.com>
2026-03-29 23:49:56 -0400

    Update send-reminders.js

--- scripts/send-reminders.js
+++ scripts/send-reminders.js
@@ -1,7 +1,4 @@
 const transport = nodemailer.createTransport({
-    host: "smtp.office365.com",
-    port: 587,
-    secure: false,
+    service: "gmail",
     auth: { user: OUTLOOK_USER, pass: OUTLOOK_PASSWORD },
-    tls: { ciphers: "SSLv3", rejectUnauthorized: false },
   });
